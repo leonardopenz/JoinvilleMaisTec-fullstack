ee289ca74de64487a9421f17fd35fb36306b9bcc
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-08-28 20:04:30 -0300

    Inicio das atividades

---
+++ m03s01/sustentavel/src/test/java/br/com/senai/sustentavel/SustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.sustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
